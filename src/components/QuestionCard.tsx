9461918ea1b7b1492784da34d50c639827604b03
MRensen <markrensen@hotmail.com>
2026-04-25 21:38:21 +0200

    finished product

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,46 @@
+import type {Question} from "../App.tsx";
+import {Field, FieldLabel} from "./ui/field.tsx";
+import {Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue} from "./ui/select.tsx";
+import he from "he"
+import {type Control, Controller, useFormContext} from "react-hook-form";
+
+export function QuestionCard({question, colors, control}:{question:Question, colors?: any , control:Control<any>}) {
+    // ${color=="green" ? "bg-green-300": color == "red"? "bg-red-300": "bg-gray-300"}
+    return (
+        <div className={`max-w-md 
+        mx-auto 
+        ${colors[question.id]}
+        rounded-sm 
+        border-2 
+        text-center 
+        my-10`}>
+            <Field key={question.id}>
+                <FieldLabel>Question: {he.decode(question.question)}</FieldLabel>
+                <Controller
+                    name={question.id}
+                    defaultValue=""
+                    control={control}
+                    render={({ field }) => (
+                        <Select onValueChange={field.onChange} value={field.value}>
+                            <SelectTrigger className="w-full max-w-48">
+                                <SelectValue placeholder="Select an answer" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>{question.type}</SelectLabel>
+                                    {question.answers.map((answer)=>{
+                                        return (
+                                            <SelectItem key={answer} value={answer}>{he.decode(answer)}</SelectItem>
+                                        )
+                                    })}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    )}
+                />
+            </Field>
+        </div>
+    )
+}
+
+export default QuestionCard;
